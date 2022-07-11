b0b0c5f165557937e70ee78a1fb69c13f3c42f12
apudo-Sebby <sebbyapudo@gmail.com>
2022-07-12 02:04:02 +0300

    Finished on dunder / constructor methods that takes parameters and attributes

--- objects.py
+++ objects.py
@@ -10,6 +10,9 @@
 
 # Creating classes
 class Dog:
+    # Constructor / Dunder methods in py
+    def __init__(self, name):
+        self.name = name
 
     def add_number(self, x):
         return x + 5
@@ -18,6 +21,8 @@ class Dog:
         print('Woof')
 
 
-d1 = Dog()
-d1.bark()
-print(d1.add_number(2))
+d1 = Dog("Seb")
+d2 = Dog('Njenga')
+
+print(d1.name)
+print(d2.name)
