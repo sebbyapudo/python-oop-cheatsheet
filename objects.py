0e300f49c3e40438ff7795490302a6a68661eb1b
apudo-Sebby <sebbyapudo@gmail.com>
2022-07-12 01:55:00 +0300

    Finished on creating a blue-print of a basic class with methods

--- objects.py
+++ objects.py
@@ -5,6 +5,19 @@
 # print(type(x))
 
 #  Accessing methods
+# string = 'Hello'
+# print(string.upper())
 
-string = 'Hello'
-print(string.upper())
+# Creating classes
+class Dog:
+
+    def add_number(self, x):
+        return x + 5
+
+    def bark(self):
+        print('Woof')
+
+
+d1 = Dog()
+d1.bark()
+print(d1.add_number(2))
