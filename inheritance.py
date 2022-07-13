493ccf86737c279b7fb0799ea76868d2a0ce43b9
apudo-Sebby <sebbyapudo@gmail.com>
2022-07-14 00:05:06 +0300

    Covered the basics pf inheritance, Inheriting from an upper level class

--- inheritance.py
+++ inheritance.py
@@ -1,1 +1,21 @@
-print('Inheritance')
+# Object oriented programming in Python
+# Inheritance Basics
+
+# Inheriting from an upper level class/ Parent class
+class Pet:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def show(self):
+        print(f'I am {self.name} and I am {self.age} years old')
+
+
+class Cat(Pet):
+    def speak(self):
+        print('Meow')
+
+
+class Dog(Pet):
+    def speak(self):
+        print('bark')
