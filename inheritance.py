8d2d3e2a8d100ff46bb1de5fbbbcf11f8e54f7b2
apudo-Sebby <sebbyapudo@gmail.com>
2022-07-14 01:00:08 +0300

    Finished on the basics of class methods

--- inheritance.py
+++ inheritance.py
@@ -41,13 +41,21 @@ class Person:
 
     def __init__(self, name):
         self.name = name
-        Person.number_of_people += 1
+        Person.add_person()
+
+    # Class Methods
+    @classmethod
+    def number_of_people_(cls):
+        return cls.number_of_people
+
+    @classmethod
+    def add_person(cls):
+        cls.number_of_people += 1
 
 
 p1 = Person('Seb')
-print(p1.number_of_people)
 p2 = Person('Jasto')
-print(p2.number_of_people)
+print(Person.number_of_people_())
 
 
 
