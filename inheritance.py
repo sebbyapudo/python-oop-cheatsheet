c2aa584523d9605b889a545efa0ae37dc0212e99
apudo-Sebby <sebbyapudo@gmail.com>
2022-07-14 00:27:35 +0300

    Covered the basics of calling an upper level class using the super method

--- inheritance.py
+++ inheritance.py
@@ -10,12 +10,25 @@ class Pet:
     def show(self):
         print(f'I am {self.name} and I am {self.age} years old')
 
-
+# Adding extra attributes to a lower level class
 class Cat(Pet):
+    def __init__(self, name, age, color):
+        super().__init__(name, age)
+        self.color = color
+
     def speak(self):
         print('Meow')
+
+    def show(self):
+        print(f'I am {self.name} and I am {self.age} years old and I am {self.color}')
 
 
 class Dog(Pet):
     def speak(self):
         print('bark')
+
+
+d1 = Dog('Bolingo', 1)
+d1.show()
+c1 = Cat('Garfield', 5, 'Gold')
+c1.show()
